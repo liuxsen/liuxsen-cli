1fe3800edcbb92ed813addca77ebf60d62a7598c
刘建辉 <jianhui@yzyw-inc.com>
2018-06-11 00:36:10 +0800

    update: init

---
+++ index.js
@@ -0,0 +1,63 @@
+#!/usr/bin/env node
+const inquirer = require('inquirer');
+const program = require('commander');
+const download = require('git-clone');
+const handlebars = require('handlebars');
+const ora = require('ora');
+const fs = require('fs');
+const chalk = require('chalk');
+
+program
+  .version('1.0.0', '-v, --version')
+  .command('init <name>')
+  .action(name => {
+    if (fs.existsSync(name)) {
+      console.log(chalk.red('项目已经存在'));
+      return;
+    }
+    inquirer
+      .prompt([
+        {
+          type: 'input',
+          name: 'description',
+          message: '请输入项目描述'
+        },
+        {
+          type: 'input',
+          name: 'author',
+          message: '请输入作者名称'
+        }
+      ])
+      .then(answers => {
+        const spinner = ora('正在下载模板...');
+        spinner.start();
+        download(
+          'https://github.com/liuxsen/cli-template',
+          `${name}`,
+          { git: 'git' },
+          err => {
+            if (!err) {
+              const meta = {
+                name,
+                description: answers.description,
+                author: answers.author
+              };
+              const fileName = `${name}/package.json`;
+              if (fs.existsSync(fileName)) {
+                const content = fs.readFileSync(fileName).toString();
+                const result = handlebars.compile(content)(meta);
+                fs.writeFileSync(fileName, result);
+                spinner.text = '';
+                spinner.succeed();
+                console.log(chalk.green('项目创建成功'));
+              }
+            } else {
+              spinner.fail();
+              console.log(chalk.red('项目创建失败'));
+              console.log(err);
+            }
+          }
+        );
+      });
+  });
+program.parse(process.argv);
